1aa4c777e396dcf720afa44c509bb25d30d742f4
wangxinyu <19482929+Bishoptylaor@users.noreply.github.com>
2025-07-02 17:10:26 +0800

    [feature]<mmarshal> init

---
+++ mmarshal/global.go
@@ -0,0 +1,58 @@
+/*
+ *  ┏┓      ┏┓
+ *┏━┛┻━━━━━━┛┻┓
+ *┃　　　━　　  ┃
+ *┃   ┳┛ ┗┳   ┃
+ *┃           ┃
+ *┃     ┻     ┃
+ *┗━━━┓     ┏━┛
+ *　　 ┃　　　┃神兽保佑
+ *　　 ┃　　　┃代码无BUG！
+ *　　 ┃　　　┗━━━┓
+ *　　 ┃         ┣┓
+ *　　 ┃         ┏┛
+ *　　 ┗━┓┓┏━━┳┓┏┛
+ *　　   ┃┫┫  ┃┫┫
+ *      ┗┻┛　 ┗┻┛
+ @Time    : 2025/7/2 -- 17:07
+ @Author  : 亓官竹 ❤️ MONEY
+ @Copyright 2025 亓官竹
+ @Description: mmarshal mmarshal/global.go
+*/
+
+package mmarshal
+
+import (
+	"errors"
+)
+
+var (
+	InvalidUnmarshalError = errors.New("v must be a non-nil struct pointer")
+	InvalidMarshalError   = errors.New("v must be of type map, map pointer, struct or struct pointer")
+	InvalidPropBytes      = errors.New("bytes are not from valid .properties config")
+	UnsupportedTypeError  = errors.New("unsupported type")
+)
+
+func Marshal(v interface{}) ([]byte, error) {
+	return marshal(v)
+}
+
+func Unmarshal(data []byte, v interface{}) error {
+	p, err := propsFromBytes(data, "")
+	if err != nil {
+		return err
+	}
+	return UnmarshalKV(p.kv, v)
+}
+
+func UnmarshalKV(kv map[string]string, v interface{}) error {
+	return unmarshalKV(kv, v)
+}
+
+func UnmarshalKey(key string, data []byte, v interface{}) error {
+	p, err := propsFromBytes(data, key+".")
+	if err != nil {
+		return err
+	}
+	return UnmarshalKV(p.kv, v)
+}
